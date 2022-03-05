21b6e5d698bdf9ba4d0a1445a05db5a810eb2ab3
Nikol <scaliariy1@gmail.com>
2022-03-05 17:05:10 +0200

    created dynamic categories

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\MainController::class, 'index']);
+Route::get('/categories', [\App\Http\Controllers\MainController::class, 'categories']);
+Route::get('/{category}', [\App\Http\Controllers\MainController::class, 'category']);
+Route::get('/mobiles/{product?}', [\App\Http\Controllers\MainController::class, 'product']);
+
