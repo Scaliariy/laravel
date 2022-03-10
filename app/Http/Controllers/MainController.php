5565aad97214547421428073cdb3c9f55827586f
Nikol <scaliariy1@gmail.com>
2022-03-10 18:22:18 +0200

    18 pagination

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,15 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
+use App\Http\Requests\ProductsFilterRequest;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index()
+    public function index(ProductsFilterRequest $request)
     {
-        $products = Product::get();
+        $productsQuery = Product::query();
+
+        if ($request->filled('price_from')) {
+            $productsQuery->where('price', '>=', $request->price_from);
+        }
+
+        if ($request->filled('price_to')) {
+            $productsQuery->where('price', '<=', $request->price_to);
+        }
+
+        foreach (['hit', 'new', 'recommend'] as $field) {
+            if ($request->has($field)) {
+                $productsQuery->where($field, 1);
+            }
+        }
+
+        $products = $productsQuery->paginate(6)->withPath("?" . $request->getQueryString());
         return view('index', compact('products'));
     }
 
