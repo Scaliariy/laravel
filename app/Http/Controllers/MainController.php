cd23cf1d3e7d9254e7eb7281b2d6db59d45aea35
Nikol <scaliariy1@gmail.com>
2022-03-15 22:45:19 +0200

    24 email

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -47,7 +47,7 @@ class MainController extends Controller
 
     public function product($category, $productCode)
     {
-        $product = Product::withTrashed()->byCode($productCode)->first();
+        $product = Product::withTrashed()->byCode($productCode)->firstOrFail();
         return view('product', compact('product'));
     }
 }
